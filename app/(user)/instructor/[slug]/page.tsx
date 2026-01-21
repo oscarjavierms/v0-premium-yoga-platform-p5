3d49d123d648cb0d6db45e4bb6400da7a4d76763
oscarjavierms <oscarjavierms@gmail.com>
2026-01-21 18:21:04 -0500

    Update page.tsx

--- app/(user)/instructor/[slug]/page.tsx
+++ app/(user)/instructor/[slug]/page.tsx
@@ -1,3 +1,14 @@
+export default async function Page({ params }: { params: { slug: string } }) {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>SLUG OK</h1>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
+    </main>
+  )
+}
+
+
+
 export const dynamic = "force-dynamic"
 export const revalidate = 0
 
