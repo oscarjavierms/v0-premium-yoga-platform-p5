49233d8236cfd65e5de3cd4c04d2143f515ca900
oscarjavierms <oscarjavierms@gmail.com>
2026-01-22 12:30:04 -0500

    Update page.tsx

--- app/(user)/perfil/page.tsx
+++ app/(user)/perfil/page.tsx
@@ -1,1 +1,9 @@
-
+export default function Perfil() {
+  return (
+    <div className="p-12">
+      <h1 className="font-serif text-4xl italic mb-4">Mi Perfil</h1>
+      <p className="text-gray-500 font-light">Aquí podrás editar tu información personal.</p>
+      {/* Contenido del perfil aquí */}
+    </div>
+  )
+}
