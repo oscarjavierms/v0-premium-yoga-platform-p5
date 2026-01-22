a341413c44ce08069cd82be5aa70813851cca754
oscarjavierms <oscarjavierms@gmail.com>
2026-01-22 17:02:20 -0500

    Update layout.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -3,9 +3,9 @@ import { UserHeader } from "@/components/layout/user-header"
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-white">
-      {/* Este componente contiene el diseño exacto de tu commit de anoche */}
+      {/* El header del commit de anoche */}
       <UserHeader /> 
-      <main className="pt-20">
+      <main>
         {children}
       </main>
     </div>
