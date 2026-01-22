56dbf691d7efee91b079423c93fc08bc9e996a0f
oscarjavierms <oscarjavierms@gmail.com>
2026-01-22 17:06:17 -0500

    Update layout.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -3,9 +3,8 @@ import { UserHeader } from "@/components/layout/user-header"
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-white">
-      {/* El header del commit de anoche */}
       <UserHeader /> 
-      <main>
+      <main className="pt-20"> {/* El padding superior evita que el menú tape el contenido */}
         {children}
       </main>
     </div>
