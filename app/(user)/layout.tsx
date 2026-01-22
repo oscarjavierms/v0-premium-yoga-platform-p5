48515b76738e29e9d18c22e31a7cfefe072d5524
oscarjavierms <oscarjavierms@gmail.com>
2026-01-22 12:53:18 -0500

    Update layout.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,3 +1,9 @@
+import React from "react"
+
 export default function UserLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen bg-[#FDFCFB]">{children}</div>
+  return (
+    <div className="min-h-screen bg-[#FDFCFB] selection:bg-black/5">
+      {children}
+    </div>
+  )
 }
