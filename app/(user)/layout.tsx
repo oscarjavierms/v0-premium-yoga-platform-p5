75492b525a6e8db97495f005df3a192bd43adc0a
oscarjavierms <oscarjavierms@gmail.com>
2026-01-22 22:55:16 -0500

    Update layout.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,19 +1,15 @@
 "use client"
 import { useState } from "react"
-import { UserHeader } from "@/components/layout/user-header"
-import { UserSidebar } from "@/components/layout/user-sidebar"
+import { UserHeader } from "../../components/layout/user-header"
+import { UserSidebar } from "../../components/layout/user-sidebar"
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   return (
     <div className="relative min-h-screen bg-[#F9F9F9]">
-      {/* Pasamos la función para abrir el menú */}
       <UserHeader onOpenSidebar={() => setIsSidebarOpen(true)} />
-      
-      {/* Pasamos el estado y la función para cerrar el menú */}
       <UserSidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
-      
       <main className="pt-20">
         {children}
       </main>
