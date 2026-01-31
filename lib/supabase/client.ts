b22edceecb453c06548697a95ee13319370c7ed5
oscarjavierms <oscarjavierms@gmail.com>
2026-01-30 23:34:31 -0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,15 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      // Esto evita que Vercel explote durante el build al generar /perfil
+      cookies: {
+        get(name: string) { return "" },
+        set(name: string, value: string, options: any) {},
+        remove(name: string, options: any) {},
+      }
+    }
   )
 }
 
